7f8a439efa85afd3ff95a86a4384660af156b507
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2024-06-28 15:28:31 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,44 +1,32 @@
-import numpy as np
-from flask import (Flask, request, render_template)
-import pickle
+import os
 
-#Create an app object using the Flask class. 
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
+
 app = Flask(__name__)
 
-#Load the trained model. (Pickle file)
-#model = pickle.load(open('models/model.pkl', 'rb'))
 
-
-#use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def index():
-    print('Request for index page received')
-    return render_template('index.html')
+   print('Request for index page received')
+   return render_template('index.html')
 
-#You can use the methods argument of the route() decorator to handle different HTTP methods.
-#GET: A GET message is send, and the server returns data
-#POST: Used to send HTML form data to the server.
-#Add Post method to the decorator to allow for form submission. 
-#Redirect to /predict page with the output
-#@app.route('/predict',methods=['POST'])
-#def predict():
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-    #int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
-    #features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
-    #prediction = model.predict(features)  # features Must be in the form [[a, b]]
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-    #output = round(prediction[0], 2)
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
-    #return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
 
-
-#When the Python interpreter reads a source file, it first defines a few special variables. 
-#For now, we care about the __name__ variable.
-#If we execute our code in the main program, like in our case here, it assigns
-# __main__ as the name (__name__). 
-#So if we want to run our code right here, we can check if __name__ == __main__
-#if so, execute it here. 
-#If we import this file (module) to another file then __name__ == app (which is the name of this python file).
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+   app.run()
