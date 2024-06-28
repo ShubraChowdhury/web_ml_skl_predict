c36abf15aa51ecdc32770a6a638b0f86e7e1d2d9
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2024-06-28 16:18:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
-model = pickle.load(open('.models/model.pkl', 'rb'))
+# model = pickle.load(open('.models/model.pkl', 'rb'))
+model = pickle.load(open(send_from_directory(os.path.join(app.root_path, 'models'),'model.pkl'),'rb'))
 
 @app.route('/')
 def index():
