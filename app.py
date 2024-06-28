61a46612c96c845b2840014b786ba4306c63371b
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2024-06-28 16:40:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import os
 import numpy as np
 import pickle
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+from flask import (Flask, redirect, render_template, request, send_from_directory, url_for)
+from azureml.core.model import Model
+
 
 app = Flask(__name__)
-model = pickle.load(open('./models/model.pkl', 'rb'))
+#model = pickle.load(open('./models/model.pkl', 'rb'))
 #model = pickle.load(open(send_from_directory(os.path.join(app.root_path, 'models'),'model.pkl'),'rb'))
+model_path = Model.get_model_path('model.pkl')
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
 
 @app.route('/')
 def index():
