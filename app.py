0ff225a64e449d1798181db7f31009260258e59d
Shubra <32674614+ShubraChowdhury@users.noreply.github.com>
2024-06-28 20:45:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,16 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
+@app.route('/predict',methods=['POST'])
+def predict():
 
+    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
+    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
+    prediction = model.predict(features)  # features Must be in the form [[a, b]]
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
 
 if __name__ == '__main__':
    app.run()
